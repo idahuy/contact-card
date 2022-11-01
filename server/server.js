16c86f48d1b4b38c34a2995c4e77ad151c640cb0
Huy <drac0123451@gmail.com>
2022-11-01 15:48:18 -0700

    changed routes for heroku

--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../client/'));
+app.use(express.static('../client/dist/'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 require('./routes/htmlRoutes.js')(app);
 
