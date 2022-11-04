aa5d700e82e5dd2b33d3ba6fa10d697b634bc76a
Huy <drac0123451@gmail.com>
2022-11-03 17:09:37 -0700

    added service worker

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -86,5 +86,11 @@ window.editCard = (e) => {
     submitBtnToUpdate = true;
 };
 
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+  navigator.serviceWorker.register('./service-worker.js');
+})};
+
 // Import CSS files
 import "../css/index.css";
